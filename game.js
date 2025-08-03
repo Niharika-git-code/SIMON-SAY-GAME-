8b26ea98a02888d992555e9b88ec745ffbfdd615
niharika <niharikajha1702@gmail.com>
2025-08-03 21:34:53 +0530

    modified

---
+++ game.js
@@ -0,0 +1,12 @@
+let gamesql={};
+let usersql=[];
+
+let started = true;
+let level =0;
+
+document.addEventListener("keypress", function(){
+    if (started==false){
+        console.log("game started ")
+        started==true;
+    }
+})
